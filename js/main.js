b3e703032ad931dc23e6e8bee691987719137491
kwakjkh <1210kwakjkh@gmail.com>
2024-07-16 15:00:13 +0900

    중간까지 완성

--- js/main.js
+++ js/main.js
@@ -15,4 +15,19 @@ window.onload = function(){
           prevEl: ".swiper-button-prev",
         },
       });
-}
+      const liveButton1 = document.querySelector(".live-bt1");
+      const liveButton2 = document.querySelector(".live-bt2");
+      const secondLive = document.querySelector(".second-live");
+      liveButton1.addEventListener("click", function(){
+        secondLive.classList.add("second")
+        liveButton1.classList.add("button1")
+        liveButton2.classList.add("button2")
+      })
+      const thirdLive = document.querySelector(".third-live");
+      const close = document.querySelector(".bt-box");
+      liveButton2.addEventListener("click", function(){
+        thirdLive.classList.add("third")
+        close.classList.add("close")
+      }
+    )
+    }
